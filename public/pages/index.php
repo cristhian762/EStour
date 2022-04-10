56b41c602894fc1d788269eb0132c8732f72d150
Cristhian <cristhian762@gmail.com>
2022-04-10 14:53:26 -0300

    header logo

--- public/pages/index.php
+++ public/pages/index.php
@@ -1,8 +1,14 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <?php echo file_get_contents('components/head.html') ?>
+  <?php echo file_get_contents('components/head.php') ?>
   
   <body>
-    <h1>Hello world 2</h1>
+    <?php echo file_get_contents('components/header.html') ?>
+    
+    <main>
+      <h1>Hello world 2</h1>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
